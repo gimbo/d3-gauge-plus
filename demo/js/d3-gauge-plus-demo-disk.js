2b6fe89c812db826784a1e63263572f4f341924a
Andy Gimblett <gimbo@gimbo.org.uk>
2013-11-28 17:04:39 +0000

    Extensive rewrite of disk demos

--- demo/js/d3-gauge-plus-demo-disk.js
+++ demo/js/d3-gauge-plus-demo-disk.js
@@ -10,55 +10,170 @@ var d3_gauge_plus_demo_disk = (function() {
 
   function diskDemos() {
 
+
+
     d3_gauge_plus.disk.createDisk({
-        name: "diskTesting1"
-      }).drawCircle(0.95, { fill: "#ccc" })
+        name: "diskEmpty"
+      });
+
+    d3_gauge_plus.disk.createDisk({
+        name: "diskEmptyNoBorder",
+        classes: "noborder"
+      });
+
+    d3_gauge_plus.disk.createDisk({
+        name: "diskEmptyRadius50",
+        radius: 50
+      });
+
+
+
+    d3_gauge_plus.disk.createDisk({
+        name: "diskCircles1",
+      }).drawCircle(1);
+
+    d3_gauge_plus.disk.createDisk({
+        name: "diskCircles2",
+      }).drawCircle(1.2, { fill: "red",
+                           stroke: "none" })
+        .drawCircle(0.8, { fill: "white",
+                           stroke: "blue",
+                           "stroke-width": "5px" })
+        .drawCircle(0.3, { fill: "green" })
+        .drawCircle(0.4, { fill: "yellow" })
+        .drawCircle(0.5, { fill: "none" });
+
+
+
+    d3_gauge_plus.disk.createDisk({
+        name: "diskStyles",
+        defaultStyles: {
+          fill: "none",
+          "stroke-width": "3px"
+        }
+      }).drawCircle(0.9)
+        .drawCircle(0.7)
+        .drawCircle(0.5, { stroke: "green",
+                           fill: "blue" })
+        .drawCircle(0.3, { fill: "yellow" })
+        .drawCircle(0.2, { stroke: "red" });
+
+
+
+    d3_gauge_plus.disk.createDisk({
+        name: "diskRadial1",
+        defaultStyles: {
+          "stroke-width": "2px"
+        }
+      }).drawRadial(0, 0, 1)
+      .drawRadial(30, 0, 1)
+      .drawRadial(60, 0, 1)
+      .drawRadial(90, 0, 1)
+      .drawRadial(180, 0.2, 0.9)
+      .drawRadial(250, 0.3, 0.8, {
+        stroke: "green",
+        "stroke-width": "10px"
+      })
+      .drawRadial(270, 0.7, 0.2);
+
+    var radial2 = d3_gauge_plus.disk.createDisk({
+        name: "diskRadial2"
+      });
+    for (var i=1; i <= 80; i++) {
+      var frac = i/80,
+        color = Math.floor(255 * (1-frac));
+      radial2.drawRadial(360 * 3 * frac, 0.68 * frac, 0.9 * frac, {
+        stroke: "rgb(255," + color + "," + color + ")"
+      });
+    }
+
+
+
+    d3_gauge_plus.disk.createDisk({
+        name: "diskArc1",
+    }).drawArc(0, 90, 0.5, 0.6)
+      .drawArc(180, 270, 0, 1, {
+        fill: "yellow",
+        stroke: "red",
+        "stroke-width": "0.5px"
+      })
+      .drawArc(135, 225, 0.3, 0.8, {
+        fill: "none"
+      })
+      .drawArc(330, 290, 1.2, 0.8, {
+        fill: "red",
+        stroke: "none"
+      })
+      .drawArc(300, 320, 0.6, 1, {
+        fill: "rgba(0, 0, 255, 0.5)"
+      });
+
+
+
+    d3_gauge_plus.disk.createDisk({
+        name: "diskText1",
+        defaultStyles: {
+          "font-family": "Helvetica, Arial"
+        }
+      }).drawText(45, 0.8, 0, 0.3, "hello")
+        .drawText(235, 0.5, 85, 0.4, "world", {
+          "font-family": "Courier New",
+          "fill": "red"
+        });
+
+    var diskText2 = d3_gauge_plus.disk.createDisk({
+        name: "diskText2",
+        radius: 150
+      });
+    for (var angle=0; angle < 360; angle += 45) {
+      diskText2.drawText(angle, 0.75, angle, 0.2, "" + angle);
+      diskText2.drawRadial(angle, 0.2, 0.75, {
+        "stroke-width": "0.5px"
+      });
+    };
+
+    var diskText3 = d3_gauge_plus.disk.createDisk({
+        name: "diskText3",
+        radius: 150
+      });
+    for (var angle=0; angle < 360; angle += 45) {
+      diskText3.drawText(angle, 0.75, 0, 0.2, "" + angle);
+      diskText3.drawRadial(angle, 0.2, 0.75, {
+        "stroke-width": "0.5px"
+      });
+    };
+
+
+
+    d3_gauge_plus.disk.createDisk({
+        name: "diskCombined",
+        radius: 200,
+        defaultStyles: {
+          fill: "blue",
+          "font-family": "Georgia",
+          "stroke-width": "3px"
+        }
+      }).drawCircle(0.98, { fill: "#ccc" })
         .drawCircle(0.85, { fill: "#fff",
                             stroke: "#ccc",
                             "stroke-width": "0.5px"})
         .drawArc(0, 90, 0.65, 0.85)
-        .drawArc(135, 310, 0.25, 0.35, { fill: "green" })
-        .drawArc(180, 210, 0.75, 0.9, { fill: "yellow" });
+        .drawText(200, 0.8, 0, 0.1, "Peekaboo")
+        .drawArc(180, 280, 0.75, 0.9, { stroke: "yellow",
+                                        "stroke-width": "2.5px",
+                                        fill: "rgba(255, 0, 255, 0.3)"
+                                      })
+        .drawCircle(0.15, { fill: "red" })
+        .drawRadial(45, 0.4, 0.7, { "stroke-width": "5px" })
+        .drawRadial(55, 0.4, 0.7, { "stroke-width": "8px" })
+        .drawRadial(305, 0.4, 0.7, { "stroke": "red" })
+        .drawRadial(315, 0.4, 0.7)
+        .drawArc(115, 245, 0.55, 0.65, { fill: "green" })
+        .drawText(45, 1.1, 45, 0.15, "Disks!");
 
-    d3_gauge_plus.disk.createDisk({
-        name: "diskTesting2",
-        radius: 150
-      }).drawCircle(0.95, { fill: "red" })
-        .drawCircle(0.85, { fill: "yellow",
-                            stroke: "#ccc",
-                            "stroke-width": "5px" })
-        .drawArc(195, 340, 0.25, 0.45, { fill: "green" })
-        .drawRadial(35, 0.4, 0.8, { "stroke-width": "2px" })
-        .drawRadial(45, 0.4, 0.8, { "stroke-width": "2px" })
-        .drawRadial(55, 0.4, 0.8, { "stroke-width": "2px" })
-        .drawRadial(65, 0.4, 0.8, { "stroke-width": "2px" });
 
-    d3_gauge_plus.disk.createDisk({
-        name: "diskTextDemo1",
-        radius: 150
-      }).drawCircle(0.95, { fill: "#fff" })
-        .drawText(0, 0.75, 0, 0.2, "0")
-        .drawText(90, 0.75, 90, 0.2, "90")
-        .drawText(180, 0.75, 180, 0.2, "180")
-        .drawText(270, 0.75, 270, 0.2, "270")
-        .drawRadial(0, 0.4, 0.7, { "stroke-width": "2px" })
-        .drawRadial(90, 0.4, 0.7, { "stroke-width": "2px" })
-        .drawRadial(180, 0.4, 0.7, { "stroke-width": "2px" })
-        .drawRadial(270, 0.4, 0.7, { "stroke-width": "2px" });
 
-    d3_gauge_plus.disk.createDisk({
-        name: "diskTextDemo2",
-        radius: 150
-      }).drawCircle(0.95, { fill: "#fff" })
-        .drawText(0, 0.75, 0, 0.2, "0")
-        .drawText(90, 0.75, 0, 0.2, "90")
-        .drawText(180, 0.75, 0, 0.2, "180")
-        .drawText(270, 0.75, 0, 0.2, "270")
-        .drawRadial(0, 0.4, 0.7, { "stroke-width": "2px" })
-        .drawRadial(90, 0.4, 0.7, { "stroke-width": "2px" })
-        .drawRadial(180, 0.4, 0.7, { "stroke-width": "2px" })
-        .drawRadial(270, 0.4, 0.7, { "stroke-width": "2px" });
-  }
+    }
 
   return {
     initialize : function() {
